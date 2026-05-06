2ae73c3669e159be74b6e760345cd9e8a5820b28
tesacek <tomas.kubicek@orbiszlin.cz>
2026-05-06 21:12:23 +0200

    přidána navigace pro všechny obrazovky aplikace

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,19 +6,22 @@ import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+    anchor: '(tabs)',
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme();
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+            <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="new-deck" options={{ headerShown: false }} />
+                <Stack.Screen name="study" options={{ headerShown: false }} />
+                <Stack.Screen name="edit-deck" options={{ headerShown: false }} />
+                <Stack.Screen name="ai-deck" options={{ headerShown: false }} />
+            </Stack>
+            <StatusBar style="auto" />
+        </ThemeProvider>
+    );
 }
